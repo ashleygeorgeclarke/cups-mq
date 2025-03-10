58d0562cf738a0357ada9ce1a000c21cc74f895e
Ash Clarke <ashleygeorgeclarke@gmail.com>
2025-03-10 13:27:33 +1000

    init

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/ashleygeorgeclarke/cups-mq
+
+go 1.23.1
+
+require (
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/phin1x/go-ipp v1.6.1 // indirect
+	github.com/rabbitmq/amqp091-go v1.10.0 // indirect
+	github.com/spf13/cobra v1.9.1 // indirect
+	github.com/spf13/pflag v1.0.6 // indirect
+)
---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bytes"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/phin1x/go-ipp"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	godotenv.Load() //dont' care if it fails
+
+	conn, err := amqp.Dial("amqp://" + os.Getenv("MQ_USERNAME") + ":" + os.Getenv("MQ_PASSWORD") + "@" + os.Getenv("MQ_HOST") + ":5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		os.Getenv("QUEUE_NAME"), // name
+		true,                    // durable
+		false,                   // delete when unused
+		false,                   // exclusive
+		false,                   // no-wait
+		nil,                     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+	client := ipp.NewIPPClient(os.Getenv("CUPS_HOST"), 631, "user", "password", true)
+
+	var forever chan struct{}
+
+	go func() {
+		for d := range msgs {
+
+			reader := bytes.NewReader(d.Body)
+
+			myDoc := ipp.Document{
+				Document: reader,
+				Size:     len(d.Body),
+				Name:     "mydoc.pdf",
+				MimeType: "application/pdf",
+			}
+
+			someCode, err := client.PrintJob(myDoc, os.Getenv("PRINTER_NAME"), map[string]interface{}{})
+
+			log.Println("jobCode", someCode)
+			if err != nil {
+				log.Println("error", err)
+			}
+
+		}
+	}()
+
+	log.Printf("[*] Waiting for printable documents. To exit press CTRL+C")
+	<-forever
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
